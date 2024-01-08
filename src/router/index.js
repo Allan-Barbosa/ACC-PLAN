b4e8d78ac044d0d3df95e2e80e9417237830bf23
Allan Barbosa <allanb3779@gmail.com>
2024-01-08 03:33:00 -0300

    corrigindo os component's do router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import minhaLista from '../views/minhaLista.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,7 +17,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('..\..\src\views\AboutView.vue')
+      component: AboutView
     },
     {
       path: '/minhaLista',
@@ -23,7 +25,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('..\..\src\views\minhaLista.vue')
+      component: minhaLista
     }
   ]
 })
